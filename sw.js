afde4b510f97e309d0613c55f5d4caaeeef4e94e
Viratvishnu13 <e.p.vishnu10@gmail.com>
2026-01-26 16:54:02 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tm-booker-v3';
+const CACHE_NAME = 'tm-booker-v4';
 const BASE_PATH = '/copy-of-toastmaster-slot-booker';
 
 // Assets to cache - using relative paths for GitHub Pages
